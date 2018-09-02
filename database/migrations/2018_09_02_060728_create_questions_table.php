95696c104b797ce920beac10b1629fc381042013
Rodney Hess <rodney.hess@gmail.com>
2018-09-02 16:55:14 -0500

    generate question model, add relationships

--- database/migrations/2018_09_02_060728_create_questions_table.php
+++ database/migrations/2018_09_02_060728_create_questions_table.php
@@ -26,7 +26,7 @@ class CreateQuestionsTable extends Migration
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            
+
         });
     }
 
