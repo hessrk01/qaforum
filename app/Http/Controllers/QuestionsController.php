4fd9c7be53650e96286864ea79e1434110de3f60
Rodney Hess <rodney.hess@gmail.com>
2018-09-03 08:36:41 -0500

    added answers, views, votes, and css classes

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -16,7 +16,7 @@ class QuestionsController extends Controller
     {
         //
         // \DB::enableQueryLog();
-        $questions = Question::with('user')->latest()->paginate(10);
+        $questions = Question::with('user')->latest()->paginate(4);
         return view('questions.index', compact('questions'));
 
         // dd(\DB::getQueryLog());
--- app/User.php
+++ app/User.php
@@ -32,6 +32,6 @@ class User extends Authenticatable
     }
 
     public function getUrlAttribute(){
-        return "#";
+        return "#"; 
     }
 }
