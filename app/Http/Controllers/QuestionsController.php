e3ad9b025af187ab9880b8bf6d455f61212a8f35
Rodney Hess <rodney.hess@gmail.com>
2018-09-04 13:57:13 -0500

    auth questions by policy

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -8,6 +8,10 @@ use App\Http\Requests\AskQuestionRequest;
 
 class QuestionsController extends Controller
 {
+    public function __construct() 
+    {
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
     /**
      * Display a listing of the resource.
      *
@@ -69,6 +73,7 @@ class QuestionsController extends Controller
      */
     public function edit(Question $question)
     {
+        $this->authorize('update', $question);
         //
         return view("questions.edit", compact('question'));
     }
@@ -82,6 +87,7 @@ class QuestionsController extends Controller
      */
     public function update(AskQuestionRequest $request, Question $question)
     {
+        $this->authorize('update', $question);
         //
         $question->update($request->only('title', 'body'));
         return redirect('/questions')->with('success', "Your question has been updated");
@@ -95,6 +101,7 @@ class QuestionsController extends Controller
      */
     public function destroy(Question $question)
     {
+        $this->authorize('delete', $question);
         //
         $question->delete();
         return redirect('/questions')->with('success', "Your question has been deleted");
