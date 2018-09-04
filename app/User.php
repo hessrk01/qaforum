93ad0541a82ed12b6c60b343815f46b9fe22631b
Rodney Hess <rodney.hess@gmail.com>
2018-09-04 14:18:41 -0500

    Answers schema, renamed answers to answers_count

--- app/User.php
+++ app/User.php
@@ -34,4 +34,9 @@ class User extends Authenticatable
     public function getUrlAttribute(){
         return "#"; 
     }
+
+    public function answers()
+    {
+        return $this->hasMany(Answer::class);
+    }
 }
